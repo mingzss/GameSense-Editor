265f049a44b012e0c9556ab21792ae61d24bc610
Mingzs <mingzsxiao@gmail.com>
2022-02-09 18:46:06 +0800

    Version 12.8.6

---
+++ Scripts/Engine/Coroutine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSEngine
+{
+    public delegate void TimedFunc();
+    class TimedFunction
+    {
+        public TimedFunc func;
+        public float timer;
+
+        public TimedFunction(TimedFunc f, float t)
+        {
+            func = f;
+            timer = t;
+        }
+    }
+
+    public static class Coroutine
+    {
+        static List<TimedFunction> timedFunctions = new List<TimedFunction>();
+
+        private static void Update(float dt)
+        {
+            for (int i = timedFunctions.Count - 1; i >= 0; i--)
+            {
+                timedFunctions[i].timer -= dt;
+                if(timedFunctions[i].timer <= 0f)
+                {
+                    timedFunctions[i].func();
+                    timedFunctions.RemoveAt(i);
+                }
+            }
+        }
+
+        public static void Execute(TimedFunc f, float delay)
+        {
+            timedFunctions.Add(new TimedFunction(f, delay));
+        }
+    }
+}
