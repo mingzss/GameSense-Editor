6428c2048df6aa35c7cdb80daa25001cbfbda165
Mingzs <mingzsxiao@gmail.com>
2022-03-06 22:16:45 +0800

    Version 13.0.6

--- Scripts/Engine/Physics.cs
+++ Scripts/Engine/Physics.cs
@@ -7,14 +7,22 @@ using System.Runtime.CompilerServices;
 
 namespace GSEngine
 {
-    class Physics
+    internal class Physics
     {
         public static GameObject RayCast2D(Vector2 Position, Vector2 Direction, ref float Distance_Out)
         {
             return new GameObject(RayCast2D_Native(ref Position, ref Direction, ref Distance_Out));
         }
 
+        public static GameObject RayCast2D_Layered(Vector2 Position, Vector2 Direction, ref float Distance_Out, string Layer)
+        {
+            return new GameObject(RayCast2D_Layered_Native(ref Position, ref Direction, ref Distance_Out, Layer));
+        }
+
         [MethodImpl(MethodImplOptions.InternalCall)]
         internal static extern ulong RayCast2D_Native(ref Vector2 Position, ref Vector2 Direction, ref float Distance_Out);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        internal static extern ulong RayCast2D_Layered_Native(ref Vector2 Position, ref Vector2 Direction, ref float Distance_Out, string Layer);
     }
 }
