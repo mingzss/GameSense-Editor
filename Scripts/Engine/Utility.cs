0d93d79c6b59859f394d61bea34ef3259af6da58
Mingzs <mingzsxiao@gmail.com>
2022-04-11 01:59:18 +0800

    Update Utility.cs

--- Scripts/Engine/Utility.cs
+++ Scripts/Engine/Utility.cs
@@ -11,7 +11,16 @@ namespace GSEngine
         //Timer has to be between 0-1, 0 is start, 1 is end
         public static float Lerp(float startValue, float endValue, float timer)
         {
+            timer = Math.Min(timer, 1f);
+            timer = Math.Max(timer, 0f);
             return startValue + (endValue - startValue) * timer;
+        }
+
+        public static int Lerp(int startValue, int endValue, float timer)
+        {
+            timer = Math.Min(timer, 1f);
+            timer = Math.Max(timer, 0f);
+            return (int)(startValue + (endValue - startValue) * timer);
         }
     }
 }
