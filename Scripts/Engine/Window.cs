19fdb739cf97b5a9e6f6fbf520994e17c3eebe78
Mingzs <mingzsxiao@gmail.com>
2022-04-11 01:47:32 +0800

    Version 13.2.1

--- Scripts/Engine/Window.cs
+++ Scripts/Engine/Window.cs
@@ -33,6 +33,9 @@ namespace GSEngine
         public static extern int GetHeight();
 
         [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void SetShowCursor(bool show);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
         private static extern void GetCenterOfScreen_Native(out Vector2 outCenter);
     }
 }
