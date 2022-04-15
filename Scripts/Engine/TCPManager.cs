960abfe82425d476799947e1018982029a9d576a
Mingzs <mingzsxiao@gmail.com>
2022-04-15 20:45:10 +0800

    Version 13.3.1

---
+++ Scripts/Engine/TCPManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace GSEngine
+{
+    public class TCPManager
+    {
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern bool Connect(string ip_addr, int port);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public static extern void Disconnect();
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        public extern bool SendData(string data);
+    }
+}
